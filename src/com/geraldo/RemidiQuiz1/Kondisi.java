9bff6c680e5bf864e390dda619c038164875b8fd
Geraldoarema <geraldoarema87@gmail.com>
2018-11-04 19:14:58 +0700

    Commit untuk remidi quiz 1

--- src/com/geraldo/RemidiQuiz1/Kondisi.java
+++ src/com/geraldo/RemidiQuiz1/Kondisi.java
@@ -4,7 +4,7 @@ public class Kondisi {
     public void takeOff() {
         System.out.println("Pesawat Lepas Landas");
     }
-    
+    //test
     public void land() {
         System.out.println("Pesawat Mendarat");
     }
